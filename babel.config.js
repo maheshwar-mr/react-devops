875e322da883de9cbce5b35efe83b4f93ba1610a
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-11 12:01:29 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/plugin-proposal-class-properties'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  module: ['@babel/plugin-proposal-class-properties']
 };
