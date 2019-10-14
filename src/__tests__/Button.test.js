2f649631ffe40d208316b4d59ba4faefdc22ccdd
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-14 15:06:33 +0530

    Update Button.test.js

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,5 +1,16 @@
 import React from "react";
 import { create } from "react-test-renderer";
+
+function Button(props) {
+  return <button>Nothing to do for now</button>;
+}
+describe("Button component", () => {
+  test("Matches the snapshot", () => {
+    const button = create(<Button />);
+    expect(button.toJSON()).toMatchSnapshot();
+  });
+});
+
 describe("Button component", () => {
   test("Matches the snapshot", () => {
     const button = create(<Button />);
