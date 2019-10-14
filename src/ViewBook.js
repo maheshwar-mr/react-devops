6e7f0b50f54503196e8c43535a4feabe3c727fed
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-14 19:31:13 +0530

    Update ViewBook.js

--- src/ViewBook.js
+++ src/ViewBook.js
@@ -205,7 +205,7 @@ class ViewBook extends Component{
     {
       return(
         
-        <div id="main">
+        <div id="main" key="{item.id}">
           <div className="container">
             <div className="card" id="now">
               <div className="card-body" >
