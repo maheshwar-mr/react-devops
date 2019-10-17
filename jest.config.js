e39a38b33a5481efea63519267b8ba3dcb754c4f
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-17 17:33:35 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,12 @@
+"jest": {	"collectCoverage":true
+  "collectCoverageFrom": [	
+    "**/*.{js,jsx}",	
+    "!**/node_modules/**",	
+    "!**/build/**",	
+  ],	
+  "coverageReporters": [	
+    "lcov",	
+    "text"	
+  ]	
+},	
 "collectCoverage":true
