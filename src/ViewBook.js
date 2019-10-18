953694818f7a353dbfaf549f01768a89c07106a3
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-18 12:52:27 +0530

    Update ViewBook.js

--- src/ViewBook.js
+++ src/ViewBook.js
@@ -48,7 +48,7 @@ class ViewBook extends Component{
   componentWillMount()
   {
           try{
-    axios.get('http://18.219.22.236:3001/Books').then(
+    axios.get('http://3.16.15.63:3001/Books').then(
       (response)=>{
       this.setState({ Books:response.data})
       })
@@ -74,7 +74,7 @@ class ViewBook extends Component{
   addBook()
   {
           try{
-    axios.post('http://18.219.22.236:3001/Books', this.state.addBookDetails).then((response)=>
+    axios.post('http://3.16.15.63:3001/Books', this.state.addBookDetails).then((response)=>
     {
       let{Books}=this.state;
       Books.push(response.data);
@@ -104,7 +104,7 @@ class ViewBook extends Component{
   deleteBook(id)
   {
           try{
-    axios.delete('http://18.219.22.236:3001/Books/' +id).then((response)=>
+    axios.delete('http://3.16.15.63:3001/Books/' +id).then((response)=>
     {
       this._refreshBooks();
     });}
@@ -128,7 +128,7 @@ class ViewBook extends Component{
       format
     }=this.state.editBookDetails;
           try{
-    axios.put('http://18.219.22.236:3001/Books/' +this.state.editBookDetails.id,
+    axios.put('http://3.16.15.63:3001/Books/' +this.state.editBookDetails.id,
     {
       title,
       author,
@@ -194,7 +194,7 @@ class ViewBook extends Component{
   _refreshBooks()
   {
           try{
-    axios.get('http://18.219.22.236:3001/Books').then((response)=>
+    axios.get('http://3.16.15.63:3001/Books').then((response)=>
     {
       this.setState({
       Books:response.data
