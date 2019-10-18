9bbae108087737e5a8d522b9bb9b4e70dd2a1e40
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-18 17:10:57 +0530

    Delete App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ViewBook from '../ViewBook';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ViewBook />, div);
-});
