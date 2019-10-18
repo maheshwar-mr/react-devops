4f21603dca524ae8e9a2d31d7b17857db2f46ed6
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-18 16:42:41 +0530

    Create App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ViewBook from '../ViewBook';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ViewBook />, div);
+});
